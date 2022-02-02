c5bd7d82e351886b840d29eba6e7b42930251837
Martin Thoma <info@martin-thoma.de>
2022-02-02 12:22:54 +0100

    Use context manager for opening files

--- image_cleaner/cli.py
+++ image_cleaner/cli.py
@@ -64,8 +64,8 @@ def remove_metadata(image_files):
     for filename in image_files:
         print("## %s" % filename)
 
-        image_file = open(filename)
-        image = PIL.Image.open(image_file)
+        with open(filename) as image_file:
+            image = PIL.Image.open(image_file)
 
         # next 3 lines strip exif
         data = list(image.getdata())
